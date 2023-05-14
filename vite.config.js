79545aaf41bf4a209c1493a5229301bdae7d9337
Zoran <greedy0101@gmail.com>
2023-05-14 17:37:02 +0800

    add css injection

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import libCss from "vite-plugin-libcss";
 
 export default defineConfig({
   build: {
@@ -10,7 +11,7 @@ export default defineConfig({
       fileName: (format) => `reactcommontool-zy.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react"],
       output: {
         globals: {
           react: "React",
@@ -18,5 +19,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), libCss()],
 });

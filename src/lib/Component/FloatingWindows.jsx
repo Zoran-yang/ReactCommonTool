7127de86cae1a1858dbaee45c237815380fa8cfe
Zoran <greedy0101@gmail.com>
2023-05-15 13:16:41 +0800

    revise FloatingWindows.jsx ( add backgroundColor attri)

--- src/lib/Component/FloatingWindows.jsx
+++ src/lib/Component/FloatingWindows.jsx
@@ -1,25 +1,39 @@
-import * as React from 'react';
+import * as React from "react";
+import Paper from "@mui/material/Paper";
 
-function FloatingWindow({children, isOpen,}) {
-
+function FloatingWindow({ children, isOpen, backgroundColor }) {
   return (
     <div>
       {isOpen && (
-        <div
-          style={{
-            position: 'absolute',
-            top: '57%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            backgroundColor: '#fff',
-            padding: '20px',
-            borderRadius: '10px',
-            boxShadow: '0px 0px 10px rgba(0,0,0,0.5)',
-            width: '65%',
-          }}
+        <Paper
+          sx={
+            backgroundColor
+              ? {
+                  position: "absolute",
+                  top: "57%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  backgroundColor: backgroundColor,
+                  padding: "20px",
+                  borderRadius: "10px",
+                  boxShadow: "0px 0px 10px rgba(0,0,0,0.5)",
+                  width: "65%",
+                }
+              : {
+                  position: "absolute",
+                  top: "57%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  backgroundColor: "#fff",
+                  padding: "20px",
+                  borderRadius: "10px",
+                  boxShadow: "0px 0px 10px rgba(0,0,0,0.5)",
+                  width: "65%",
+                }
+          }
         >
-          {children}           
-        </div>
+          {children}
+        </Paper>
       )}
     </div>
   );
